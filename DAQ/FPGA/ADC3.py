7f3ca52af0e0469833f8b839a2aec42863f97a14
Willd231 <dellinger.w3@gmail.com>
2024-12-16 14:32:08 -0500

    changes

--- DAQ/FPGA/ADC3.py
+++ DAQ/FPGA/ADC3.py
@@ -1,18 +1,19 @@
-#!/home/wdellinger/anaconda3/bin/python
+#!/home/anish/anaconda3/bin/python
 import socket
 import numpy as np
 import time
+import sys
 
-nConamp = 6000
-Nsamp = 49.152
-ADC = ['ADCA', 'ADCB', 'ADCC', 'ADCD']
-ADCatt = ['0', '0', '0', '0']
-
-t = input("How long would you like this program to run (hrs): ")
-t = int(t) * 60 * 60  # Convert hours to seconds
-
-ipad = "10.17.16.10"
-packetsize = 8256
+nConamp=6000
+t = float(input("How long would you like this program (hrs): "))
+t = t * 60 * 60
+Nsamp=49.152 
+ADC=['ADCA', 'ADCB', 'ADCC', 'ADCD']
+ADCatt=['0','0','0','0'] 
+curr = time.time()
+est = input("How long do you want each iteration to be?: ")
+ipad="10.17.16.10"
+packetsize=8256
 receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 receive.bind((ipad, 60000))
 
@@ -23,24 +24,21 @@ try:
         
         while time.time() - start_time < t:
             curr1 = time.time()
+            output.write(np.array([curr1], dtype=np.float64).tobytes())
             data, address = receive.recvfrom(packetsize)
             sintval = np.frombuffer(data, dtype=np.int16)
             for adccnt in range(4):
                 adcdat = sintval[(32 + adccnt)::4]
-
-            
-            adcdat = adcdat / nConamp
-            ft = np.fft.fft(adcdat) / len(adcdat)
-            Ps = np.real(ft * np.conj(ft))
-            freq = np.arange(len(Ps)) * Nsamp / len(Ps)
-            output.write(Ps.tobytes())
+                adcdat = adcdat / nConamp
+                ft = np.fft.fft(adcdat) / len(adcdat)
+                Ps = np.real(ft * np.conj(ft))
+                freq = np.arange(len(Ps)) * Nsamp / len(Ps)
+                output.write(Ps.tobytes())
 
             curr2 = time.time()
-            curr3 = curr2 - curr1  # Time taken for the loop iteration
+            curr3 = curr2 - curr1  
             
-            output.write(np.array([curr3], dtype=np.float64).tobytes())
-
-            time.sleep(max(0, 1 - curr3))
+            time.sleep(max(0,  est - curr3))
 
             print(f"Iteration: {count}, Time taken: {curr3:.4f}s")
             count += 1
@@ -51,3 +49,4 @@ except KeyboardInterrupt:
 finally:
     output.close()
     receive.close()
+    
