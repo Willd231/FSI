bac710f4e0a221849d420c056ad8f90e8a06c9ec
seb1124 <sebastian112403@gmail.com>
2025-05-06 18:13:57 -0400

    fix

--- CUDA/goatedFilereader.h
+++ CUDA/goatedFilereader.h
@@ -1,43 +1,65 @@
 
 #ifndef goatedFilereader_H
 #define goatedFilereader_H
- 
-typedef struct packets{
-int16_t * adc1, *adc2, *adc3, *adc4;
-}
-packets;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <iostream>
+#include <fstream>
+#include <vector> 
+
+using namespace std; 
 
 
+typedef struct packet{
+//int16_t * adc1, *adc2, *adc3, *adc4;
+    std::vector<int16_t> adc1, adc2, adc3, adc4;
+} packet;
 
 
-inline readFile(char * filename)
-
+inline std::vector<packet> readFile(char * filename)
 {
-FILE * fp = fopen(fp, "rb");
-packets * new = malloc(sizeof(packets));
- 
-int16 ** return = malloc(sizeof(
-int filesize = 0;
-fp.seekg(0, std::ios::end);
-size_t filesize = fp.tellg();
-fp.seekg(0, std::ios::beg);
-size_t numPackets = filesize/ 8256;
+    
+    ifstream file(filename);
+    
+    //fp.seekg(0, std::ios::end);
+    file.seekg(0, std::ios::end);
 
-new->adc1 = malloc(sizeof(int16_t) * 1024 * numPackets);
-new->adc2 = malloc(sizeof(int16_t)* 1024 * numPackets);
-new->adc3 = malloc(sizeof(int16_t)* 1024 * numPackets);
-new->adc4 = malloc(sizeof(int16_t)* 1024 * numPackets); 
+    size_t filesize = file.tellg();
+    //fp.seekg(0, std::ios::beg);
+    file.seekg(0, std::ios::beg);
 
-for(int i = 0; i < numPackets; i++){
-for (int j = 0; j < 1024; j++){
-fp.read(reinterpret_cast<char*>(&(new->adc1[j])), sizeof(int16_t));
-fp.read(reinterpret_cast<char*>(&(new->adc2[j])), sizeof(int16_t));
-fp.read(reinterpret_cast<char*>(&(new->adc3[j])), sizeof(int16_t));
-fp.read(reinterpret_cast<char*>(&(new->adc4[j])), sizeof(int16_t));
+    size_t numPackets = filesize/ 8256;
 
+    //packet * packets = (packet *)malloc(sizeof(packet) * numPackets);
+    std::vector<packet> packets(numPackets);
+
+    for(int i = 0; i < numPackets; i++){
+        // packets[i].adc1 = (int16_t *)malloc(sizeof(int16_t) * 1024);
+        // packets[i].adc2 = (int16_t *)malloc(sizeof(int16_t) * 1024);
+        // packets[i].adc3 = (int16_t *)malloc(sizeof(int16_t) * 1024);
+        // packets[i].adc4 = (int16_t *)malloc(sizeof(int16_t) * 1024);
+        packets[i].adc1.resize(1024);
+        packets[i].adc2.resize(1024);
+        packets[i].adc3.resize(1024);
+        packets[i].adc4.resize(1024);
+
+    }
+
+
+    for(int i = 0; i < numPackets; i++){
+        for (int j = 0; j < 1024; j++){
+            file.read(reinterpret_cast<char*>(&(packets[i].adc1[j])), sizeof(int16_t));
+            file.read(reinterpret_cast<char*>(&(packets[i].adc2[j])), sizeof(int16_t));
+            file.read(reinterpret_cast<char*>(&(packets[i].adc3[j])), sizeof(int16_t));
+            file.read(reinterpret_cast<char*>(&(packets[i].adc4[j])), sizeof(int16_t));
+
+        }
+    }
+
+    return packets;
 }
-}
-
 
 
 #endif
