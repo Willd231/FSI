67df76e65a52ceadc22fbcd53061dda0d4f57d3f
Willd231 <dellinger.w3@gmail.com>
2025-08-26 14:46:56 -0400

    Major Changes to Median Filter (Python Version)

--- Processing/Filtering/MF.py
+++ Processing/Filtering/MF.py
@@ -1,35 +1,47 @@
-			
+#!/usr/bin/python3
+
+def getData(filename):
+    data = []
+    with open(filename, "rb+") as fp:
+        while fp != null:
+            data.append(fp.read())
+    return data
+
+
+def median(A, B, C):
+    sortedlist = sorted([A, B, C])
+    return sortedlist[1]
+
+
+def doFilter(Data):
+    Data.insert(0,0)
+    Filtered_Data = []
+    for i in range(0, len(Data)):
+        median = median(Data[i],Data[i+1], Data[i+2])
+        Filtered_Data.append(median)
+
+def writeFiltered(Filtered_Data):
+    with open(Filtered_Data.dat, "wb+") as f:
+        f.write(Data)
+
+
+
+def Main(argc, argv):
+    data = getData(argv[1])
+    filtered_data = doFilter(data)
+    writeFiltered(filtered_data)   
+
+
+
+
+if " __name__" == "Main":
+    Main()
 
 
 
 
 
 
-			if(FOR[0] == FOR[1] == FOR[2]){
-                                median = FOR[0];
-                        }
-                        if(FOR[0] > FOR[1] == FOR[2]){
-                                median = FOR[1];
-                        }
-                        if(FOR[0] < FOR[1] == FOR[2]){
-                                median = FOR[1];
-                        }
-                        if(FOR[0] == FOR[1] > FOR[2]){
-                                median = FOR[0];
-                        }
-                        if(FOR[0] == FOR[1] < FOR[2]){
-                                median = FOR[0];
-                        }
-                        if(FOR[0] > FOR[1] < FOR[2]){
-                                median = FOR[2];
-                        }
-                        if(FOR[0] < FOR[1] > FOR[2]){
-                                median = FOR[0];
-                        }
-                        if(FOR[0] < FOR[1] < FOR[2]){
-                                median = FOR[1];
-                        }
-                        if(FOR[0] > FOR[1] > FOR[2]){
-                                median = FOR[1];
-                        }
+
+
 
